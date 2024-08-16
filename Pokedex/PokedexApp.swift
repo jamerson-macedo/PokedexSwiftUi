e973155e12f3eb11863ae1c00ad83797aad399ff
jamerson-macedo <jamerson_macedo@hotmail.com>
2024-08-15 22:08:37 -0300

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Jamerson Macedo on 15/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
